2f16aff66f2542ec901e721dd6165e9362c81ad6
srsekhar9661 <srsekhar9661@gmail.com>
2026-02-27 11:32:10 +0530

    Engance galleru with cinematic hover effect and overlay  animation

--- src/components/PaintingCard.jsx
+++ src/components/PaintingCard.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 
-function PaintingCard({ painting }){
-    return (
-        <Link to={`/gallery/${painting.id}`} className="painting-card">
-            <img src={painting.image} alt={painting.title} />
-            <h3>{painting.title}</h3>
-        </Link>
-    )
+function PaintingCard({ painting }) {
+  return (
+    <Link to={`/gallery/${painting.id}`} className="painting-card">
+      <div className="image-wrapper">
+        <img src={painting.image} alt={painting.title} />
+        <div className="overlay">
+          <h3>{painting.title}</h3>
+        </div>
+      </div>
+    </Link>
+  );
 }
-
 
 export default PaintingCard;
--- src/data/paintings.js
+++ src/data/paintings.js
@@ -19,6 +19,46 @@ const paintings = [
         title : "Urban Shadows",
         image : "https://picsum.photos/400/500?random=4",
     },
+    {
+        id : 5,
+        title : "Sunset Horizon",
+        image : "https://picsum.photos/400/500?random=5",
+    },
+    {
+        id : 6,
+        title : "Silent Forest",
+        image : "https://picsum.photos/400/500?random=6",
+    },
+    {
+        id : 7,
+        title : "Golden Fields",
+        image : "https://picsum.photos/400/500?random=7",
+    },
+    {
+        id : 8,
+        title : "Urban Shadows",
+        image : "https://picsum.photos/400/500?random=8",
+    },
+    {
+        id : 9,
+        title : "Sunset Horizon",
+        image : "https://picsum.photos/400/500?random=9",
+    },
+    {
+        id : 10,
+        title : "Silent Forest",
+        image : "https://picsum.photos/400/500?random=10",
+    },
+    {
+        id : 11,
+        title : "Golden Fields",
+        image : "https://picsum.photos/400/500?random=11",
+    },
+    {
+        id : 12,
+        title : "Urban Shadows",
+        image : "https://picsum.photos/400/500?random=12",
+    },
 ]
 
 
