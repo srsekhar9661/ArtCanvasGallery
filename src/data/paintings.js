505d51d3a471110538d68cf7d7ef04d1e7b25b5e
srsekhar9661 <srsekhar9661@gmail.com>
2026-03-02 01:59:56 +0530

    removed the tiltles from the data file

--- src/data/paintings.js
+++ src/data/paintings.js
@@ -1,67 +1,54 @@
 const paintings = [
     {
         id : 1,
-        title : "Sunset Horizon",
         image : "/images/art_of_caliography.jpg",
     },
     {
         id : 2,
-        title : "Silent Forest",
         image : "/images/devotional_5.jpg",
     },
     {
         id : 3,
-        title : "Golden Fields",
         image : "/images/devotional_6.jpg",
     },
     {
         id : 4,
-        title : "Urban Shadows",
         image : "/images/devotional_7.jpg",
     },
     {
         id : 5,
-        title : "Sunset Horizon",
         image : "/images/devotional_8.jpg",
     },
     {
         id : 6,
-        title : "Silent Forest",
         image : "/images/devotional_9.jpg",
     },
     {
         id : 7,
-        title : "Golden Fields",
         image : "/images/devotional.jpg",
     },
     {
         id : 8,
-        title : "Urban Shadows",
         image : "/images/music_360.png",
     },
     {
         id : 9,
-        title : "Sunset Horizon",
         image : "/images/nature_2.jpg",
     },
     {
         id : 10,
-        title : "Silent Forest",
         image : "/images/nature_3.jpg",
     },
     {
         id : 11,
-        title : "Golden Fields",
         image : "/images/nature_4.jpg",
     },
     {
         id : 12,
-        title : "Urban Shadows",
         image : "/images/nature.jpg",
     },
     {
         id : 13,
-        title : "Urban Shadows",
         image : "/images/spiritual_2.jpg",
     },
 ]
